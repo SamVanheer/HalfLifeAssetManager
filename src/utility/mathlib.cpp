d799787485b5b66a55945df4ca1039ed8bed1afe
Sam <duo929@hotmail.com>
2016-04-21 16:10:14 +0200

    Converted mathlib to C++, parts updated to GLM
    Converted mathlib to C++
    Updated some function to use GLM

--- src/utility/mathlib.cpp
+++ src/utility/mathlib.cpp
@@ -16,27 +16,10 @@
 
 
 /* #include "cmdlib.h" */
-#ifndef true
-#define true 1
-#endif /* true */
-#ifndef false
-#define false 0
-#endif /* false */
+
+#include <glm/geometric.hpp>
 
 #include "mathlib.h"
-
-double VectorLength(vec3_t v)
-{
-	int		i;
-	double	length;
-	
-	length = 0;
-	for (i=0 ; i< 3 ; i++)
-		length += v[i]*v[i];
-	length = sqrt (length);		// FIXME
-
-	return length;
-}
 
 
 int VectorCompare (vec3_t v1, vec3_t v2)
@@ -50,84 +33,46 @@ int VectorCompare (vec3_t v1, vec3_t v2)
 	return true;
 }
 
-vec_t Q_rint (vec_t in)
+bool VectorCompare( const glm::vec3& lhs, const glm::vec3& rhs )
 {
-	return floor (in + 0.5);
+	for( size_t i = 0; i < 3; ++i )
+	{
+		if( fabs( lhs[ i ] - rhs[ i ] ) > EQUAL_EPSILON )
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
-void VectorMA (vec3_t va, double scale, vec3_t vb, vec3_t vc)
-{
-	vc[0] = va[0] + scale*vb[0];
-	vc[1] = va[1] + scale*vb[1];
-	vc[2] = va[2] + scale*vb[2];
-}
-
-void CrossProduct (vec3_t v1, vec3_t v2, vec3_t cross)
-{
-	cross[0] = v1[1]*v2[2] - v1[2]*v2[1];
-	cross[1] = v1[2]*v2[0] - v1[0]*v2[2];
-	cross[2] = v1[0]*v2[1] - v1[1]*v2[0];
-}
-
-vec_t _DotProduct (vec3_t v1, vec3_t v2)
-{
-	return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
-}
-
-void _VectorSubtract (vec3_t va, vec3_t vb, vec3_t out)
-{
-	out[0] = va[0]-vb[0];
-	out[1] = va[1]-vb[1];
-	out[2] = va[2]-vb[2];
-}
-
-void _VectorAdd (vec3_t va, vec3_t vb, vec3_t out)
-{
-	out[0] = va[0]+vb[0];
-	out[1] = va[1]+vb[1];
-	out[2] = va[2]+vb[2];
-}
-
-void _VectorCopy (vec3_t in, vec3_t out)
-{
-	out[0] = in[0];
-	out[1] = in[1];
-	out[2] = in[2];
-}
-
-void _VectorScale (vec3_t v, vec_t scale, vec3_t out)
-{
-	out[0] = v[0] * scale;
-	out[1] = v[1] * scale;
-	out[2] = v[2] * scale;
-}
-
-vec_t VectorNormalize (vec3_t v)
+vec_t VectorNormalize( glm::vec3& vector )
 {
 	int		i;
 	double	length;
 
-if ( fabs(v[1] - 0.000215956) < 0.0001)
-i=1;
+	//Divide by 0 protection?
+	if( fabs( vector[ 1 ] - 0.000215956 ) < 0.0001 )
+		i = 1;
 
 	length = 0;
-	for (i=0 ; i< 3 ; i++)
-		length += v[i]*v[i];
-	length = sqrt (length);
-	if (length == 0)
+	for( i = 0; i< 3; i++ )
+		length += vector[ i ] * vector[ i ];
+	length = sqrt( length );
+	if( length == 0 )
 		return 0;
-		
-	for (i=0 ; i< 3 ; i++)
-		v[i] /= length;	
+
+	for( i = 0; i< 3; i++ )
+		vector[ i ] /= length;
 
 	return length;
 }
 
-void VectorInverse (vec3_t v)
+void VectorMA( const glm::vec3& va, double scale, const glm::vec3& vb, glm::vec3& vc )
 {
-	v[0] = -v[0];
-	v[1] = -v[1];
-	v[2] = -v[2];
+	vc[ 0 ] = va[ 0 ] + scale * vb[ 0 ];
+	vc[ 1 ] = va[ 1 ] + scale * vb[ 1 ];
+	vc[ 2 ] = va[ 2 ] + scale * vb[ 2 ];
 }
 
 void ClearBounds (vec3_t mins, vec3_t maxs)
@@ -151,7 +96,7 @@ void AddPointToBounds (vec3_t v, vec3_t mins, vec3_t maxs)
 	}
 }
 
-void AngleMatrix (const vec3_t angles, float (*matrix)[4] )
+void AngleMatrix (const vec3_t angles, float matrix[ 3 ][ 4 ] )
 {
 	float		angle;
 	float		sr, sp, sy, cr, cp, cy;
@@ -239,15 +184,19 @@ void R_ConcatTransforms (const float in1[3][4], const float in2[3][4], float out
 				in1[2][2] * in2[2][3] + in1[2][3];
 }
 
-
-
-void VectorRotate (const vec3_t in1, const float in2[3][4], vec3_t out)
+void VectorRotate( const glm::vec3& vector, const glm::mat3x4& matrix, glm::vec3& outResult )
 {
-	out[0] = DotProduct(in1, in2[0]);
-	out[1] = DotProduct(in1, in2[1]);
-	out[2] = DotProduct(in1, in2[2]);
+	outResult[ 0 ] = glm::dot( vector, *reinterpret_cast<const glm::vec3*>( &matrix[ 0 ] ) );
+	outResult[ 1 ] = glm::dot( vector, *reinterpret_cast<const glm::vec3*>( &matrix[ 1 ] ) );
+	outResult[ 2 ] = glm::dot( vector, *reinterpret_cast<const glm::vec3*>( &matrix[ 2 ] ) );
 }
 
+void VectorIRotate( const glm::vec3& vector, const glm::mat3x4& matrix, glm::vec3& outResult )
+{
+	outResult[ 0 ] = vector[ 0 ] * matrix[ 0 ][ 0 ] + vector[ 1 ] * matrix[ 1 ][ 0 ] + vector[ 2 ] * matrix[ 2 ][ 0 ];
+	outResult[ 1 ] = vector[ 0 ] * matrix[ 0 ][ 1 ] + vector[ 1 ] * matrix[ 1 ][ 1 ] + vector[ 2 ] * matrix[ 2 ][ 1 ];
+	outResult[ 2 ] = vector[ 0 ] * matrix[ 0 ][ 2 ] + vector[ 1 ] * matrix[ 1 ][ 2 ] + vector[ 2 ] * matrix[ 2 ][ 2 ];
+}
 
 // rotate by the inverse of the matrix
 void VectorIRotate (const vec3_t in1, const float in2[3][4], vec3_t out)
--- src/utility/mathlib.h
+++ src/utility/mathlib.h
@@ -15,9 +15,9 @@
 
 #include <math.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat3x4.hpp>
 
 #ifdef DOUBLEVEC_T
 typedef double vec_t;
@@ -40,48 +40,72 @@ typedef vec_t vec4_t[4];	// x,y,z,w
 
 int VectorCompare (vec3_t v1, vec3_t v2);
 
+/**
+*	Compares the given vectors.
+*	@param lhs
+*	@param rhs
+*	@return true if the vectors are equal, to within EQUAL_EPSILON difference for all components, false otherwise.
+*/
+bool VectorCompare( const glm::vec3& lhs, const glm::vec3& rhs );
+
 #define DotProduct(x,y) ((x)[0]*(y)[0]+(x)[1]*(y)[1]+(x)[2]*(y)[2])
-#define VectorFill(a,b) { (a)[0]=(b); (a)[1]=(b); (a)[2]=(b);}
-#define VectorAvg(a) ( ( (a)[0] + (a)[1] + (a)[2] ) / 3 )
-#define VectorSubtract(a,b,c) {(c)[0]=(a)[0]-(b)[0];(c)[1]=(a)[1]-(b)[1];(c)[2]=(a)[2]-(b)[2];}
-#define VectorAdd(a,b,c) {(c)[0]=(a)[0]+(b)[0];(c)[1]=(a)[1]+(b)[1];(c)[2]=(a)[2]+(b)[2];}
 #define VectorCopy(a,b) {(b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2];}
 #define VectorScale(a,b,c) {(c)[0]=(b)*(a)[0];(c)[1]=(b)*(a)[1];(c)[2]=(b)*(a)[2];}
 
-vec_t Q_rint (vec_t in);
-vec_t _DotProduct (vec3_t v1, vec3_t v2);
-void _VectorSubtract (vec3_t va, vec3_t vb, vec3_t out);
-void _VectorAdd (vec3_t va, vec3_t vb, vec3_t out);
-void _VectorCopy (vec3_t in, vec3_t out);
-void _VectorScale (vec3_t v, vec_t scale, vec3_t out);
+/**
+*	Sets all of the given vector's components to the given value.
+*	@param vec Vector to fill.
+*	@param flValue Value to use.
+*/
+inline void VectorFill( glm::vec3& vec, const vec_t flValue )
+{
+	vec[ 0 ] = vec[ 1 ] = vec[ 2 ] = flValue;
+}
 
-double VectorLength(vec3_t v);
+/**
+*	Normalizes a vector in place and returns the old length.
+*/
+vec_t VectorNormalize( glm::vec3& vector );
 
-void VectorMA (vec3_t va, double scale, vec3_t vb, vec3_t vc);
+/**
+*	Computes the average of the given vector's components.
+*	@param vec Vector whose components to average.
+*	@return Average.
+*/
+inline constexpr vec_t VectorAvg( const glm::vec3& vec )
+{
+	return ( vec.x + vec.y + vec.z ) / 3;
+}
 
-void CrossProduct (vec3_t v1, vec3_t v2, vec3_t cross);
-vec_t VectorNormalize (vec3_t v);
-void VectorInverse (vec3_t v);
-
-void ClearBounds (vec3_t mins, vec3_t maxs);
-void AddPointToBounds (vec3_t v, vec3_t mins, vec3_t maxs);
+/**
+*	Performs the operation vc = va + scale * vb
+*	@param va
+*	@param scale
+*	@param vb
+*	@param vc Result.
+*/
+void VectorMA( const glm::vec3& va, double scale, const glm::vec3& vb, glm::vec3& vc );
 
 void AngleMatrix (const vec3_t angles, float matrix[3][4] );
 void AngleIMatrix (const vec3_t angles, float matrix[3][4] );
 void R_ConcatTransforms (const float in1[3][4], const float in2[3][4], float out[3][4]);
 
 void VectorIRotate (const vec3_t in1, const float in2[3][4], vec3_t out);
-void VectorRotate (const vec3_t in1, const float in2[3][4], vec3_t out);
+
+/**
+*	Rotates a vector with the given matrix.
+*/
+void VectorRotate( const glm::vec3& vector, const glm::mat3x4& matrix, glm::vec3& outResult );
+
+/**
+*	Rotates a vector by the inverse of the given matrix.
+*/
+void VectorIRotate( const glm::vec3& vector, const glm::mat3x4& matrix, glm::vec3& outResult );
 
 void VectorTransform (const vec3_t in1, const float in2[3][4], vec3_t out);
 
 void AngleQuaternion( const vec3_t angles, vec4_t quaternion );
 void QuaternionMatrix( const vec4_t quaternion, float (*matrix)[4] );
 void QuaternionSlerp( const vec4_t p, vec4_t q, float t, vec4_t qt );
-
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
