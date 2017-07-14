2aa90303d04a1d677a2f2fd4f3e8b58db208dd64
Sam <duo929@hotmail.com>
2017-07-14 13:27:35 +0200

    Add studiohdr_t::flags values

--- src/engine/shared/studiomodel/studio.h
+++ src/engine/shared/studiomodel/studio.h
@@ -58,6 +58,25 @@ enum
 	STUDIO_ATTACH_NUM_VECTORS		= 3
 };
 
+/**
+*	@brief studiohdr_t::flags values
+*	@see studiohdr_t::flags
+*/
+enum
+{
+	EF_ROCKET			=	1,			//! leave a trail
+	EF_GRENADE			=	2,			//! leave a trail
+	EF_GIB				=	4,			//! leave a trail
+	EF_ROTATE			=	8,			//! rotate (bonus items)
+	EF_TRACER			=	16,			//! green split trail
+	EF_ZOMGIB			=	32,			//! small blood trail
+	EF_TRACER2			=	64,			//! orange split trail + rotate
+	EF_TRACER3			=	128,		//! purple trail
+	EF_NOSHADELIGHT		=	256,		//! No shade lighting
+	EF_HITBOXCOLLISIONS =	512,		//! Use hitbox collisions
+	EF_FORCESKYLIGHT	=	1024,		//! Forces the model to be lit by skybox lighting
+};
+
 // header for demand loaded sequence group data
 struct studioseqhdr_t
 {
