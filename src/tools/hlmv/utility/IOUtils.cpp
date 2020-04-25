d8a4c8d4d52f5f53c8b0ca9ffd005eb4d80ed076
Sam <duo929@hotmail.com>
2020-04-25 20:13:17 +0200

    Remove unnecessary try catch

--- src/tools/hlmv/utility/IOUtils.cpp
+++ src/tools/hlmv/utility/IOUtils.cpp
@@ -134,17 +134,10 @@ FILE* utf8_fopen(const char* filename, const char* mode)
 #ifdef WIN32
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
 
-	try
-	{
-		auto wideFilename = convert.from_bytes(filename);
-		auto wideMode = convert.from_bytes(mode);
+	auto wideFilename = convert.from_bytes(filename);
+	auto wideMode = convert.from_bytes(mode);
 
-		return _wfopen(wideFilename.c_str(), wideMode.c_str());
-	}
-	catch (const std::exception& e)
-	{
-		auto error = e.what();
-	}
+	return _wfopen(wideFilename.c_str(), wideMode.c_str());
 #else
 	return fopen(filename, mode);
 #endif
