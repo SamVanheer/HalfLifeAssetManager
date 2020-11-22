c5362bfeb348341ab35d92fe91beef7b092e6042
Solokiller <duo929@hotmail.com>
2020-11-22 19:05:00 +0100

    Remove old settings & keyvalues code

--- src/tools/hlmv/utility/IOUtils.h
+++ src/tools/hlmv/utility/IOUtils.h
@@ -3,22 +3,6 @@
 
 #include <cstdio>
 
-#include "keyvalues/KVForward.h"
-
-class Color;
-
-bool LoadColorSetting( const kv::Block& settings, const char* const pszName, Color& color, const bool bHasAlpha = false );
-
-bool SaveColorSetting( kv::Writer& writer, const char* const pszName, const Color& color, const bool bHasAlpha = false );
-
-bool LoadColorCVarSetting( const kv::Block& settings, const char* const pszName, const char* const pszCVar, const bool bHasAlpha = false );
-
-bool SaveColorCVarSetting( kv::Writer& writer, const char* const pszName, const char* const pszCVar, const bool bHasAlpha = false );
-
-bool LoadArchiveCVars( const kv::Block& cvars );
-
-bool SaveArchiveCVars( kv::Writer& writer, const char* const pszBlockName );
-
 /**
 *	@brief Wrapper around fopen to open filenames containing UTF8 characters on Windows
 */
