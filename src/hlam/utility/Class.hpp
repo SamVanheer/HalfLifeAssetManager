6d7d738920a661b9119167535dd0640fbdcdb7c3
Sam V <sam.vanheer@outlook.com>
2022-12-15 21:16:04 +0100

    Add hint file to suggest to Intellisense what macros do

--- src/hlam/utility/Class.hpp
+++ src/hlam/utility/Class.hpp
@@ -3,12 +3,12 @@
 /**
 *	@brief This macro defines ThisClass to be the current class. Used in a class declaration for a class with no base.
 */
-#define DECLARE_CLASS_NOBASE( thisClass )	\
+#define DECLARE_CLASS_NOBASE(thisClass)	\
 using ThisClass = thisClass
 
 /**
 *	@brief This macro defines ThisClass to be the current class, and BaseClass to be the base class. Used in a class declaration.
 */
-#define DECLARE_CLASS( thisClass, baseClass )	\
-DECLARE_CLASS_NOBASE( thisClass );				\
+#define DECLARE_CLASS(thisClass, baseClass)	\
+DECLARE_CLASS_NOBASE(thisClass);			\
 using BaseClass = baseClass
